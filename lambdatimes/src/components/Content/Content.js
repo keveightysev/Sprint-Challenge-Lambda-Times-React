2040e4edb8579b3d9ee8c87575c9dd6afa668a71
Kevin Smith <keveightysev@gmail.com>
2019-02-22 12:22:26 -0600

    Added working carousel

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel'
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -54,6 +55,7 @@ export default class Content extends Component {
     return (
       <div className="content-container">
         <Tabs tabs={this.state.tabs} selectedTab={this.state.selected} selectTabHandler={this.changeSelected} />
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
